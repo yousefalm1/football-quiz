bdd6a04b6600881af5918ed7b37be4e88db332c8
yousefalm1 <yousefalm@outlook.com>
2023-06-22 16:36:53 +0300

    Add three variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
 const questions = [
     {
-        question: jhasihbfaisbvfjsbi
+        question: 'what team is the most popular team In the UK',
+        answers: [
+            {Text: 'Man United', correct : true},
+            {Text: 'Arsenal', correct : false},
+            {Text: 'Liverpool', correct : false},
+            {Text: 'Man City', correct: false}
+
+        ],
+
+        question: 'what team is the most popular team In the UK',
+        answers: [
+            {Text: 'Man United', correct : true},
+            {Text: 'Arsenal', correct : false},
+            {Text: 'Liverpool', correct : false},
+            {Text: 'Man City', correct: false}
+
+        ],
+
     }
 ]
+
+const question = document.getElementById("question");
+const answerButton = document.getElementsByClassName("options-btn");
+const nextButton  = document.getElementsByClassName("next-container button")
